5b9015de2ed2dc69a66d488bce54e10d80ca6654
Risalord <max.hej777@gmail.com>
2022-09-16 10:57:16 +0200

    Added speed Limit

--- Max_Petersson_Processing/Assets/Scripts/Assignment4.cs
+++ Max_Petersson_Processing/Assets/Scripts/Assignment4.cs
@@ -6,9 +6,10 @@ public class Assignment4 : ProcessingLite.GP21
 {
     private Vector2 circlePosition = Vector2.zero;
     private Vector2 mousePosition;
-    private Vector2 startPosition;
     private Vector2 direction = Vector2.zero;
-   
+
+    private float maxSpeed = 0.04f;
+    private float radius = 2f;
     private float speed = 2f; // initiallisera till kanske 1 eller något
 
     // Start is called before the first frame update
@@ -20,12 +21,13 @@ public class Assignment4 : ProcessingLite.GP21
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Background(0);
 
-        Circle(circlePosition.x, circlePosition.y, 2f);
+        Circle(circlePosition.x, circlePosition.y, radius);
        
 
         if (Input.GetMouseButtonDown(0))
         {
             circlePosition = Vector2.zero;
+            direction = Vector2.zero;
             circlePosition = mousePosition;
             
         }
@@ -43,12 +45,40 @@ public class Assignment4 : ProcessingLite.GP21
         }
         if (Input.GetMouseButtonUp(0))
         {
+            
             direction = (mousePosition - circlePosition) * speed * Time.deltaTime;
             
         }
+        //circelns rörelse
+       //0.04 och 0.04
+        if(direction.x > maxSpeed)
+        {
+            direction = direction.normalized * maxSpeed; 
+        }
+        else if(direction.x < maxSpeed * -1f)
+        {
+            direction = direction.normalized * maxSpeed;
+        }
+        if (direction.y > maxSpeed)
+        {
+            direction = direction.normalized * maxSpeed;
+        }
+        else if(direction.y < maxSpeed * -1f)
+        {
+            direction = direction.normalized * maxSpeed;
+        }
 
         circlePosition = (direction + circlePosition);
-
+       
+        
+        if(circlePosition.x > Width-radius || circlePosition.x <  radius)
+        {
+            direction.x *= -1f;
+        }
+        if(circlePosition.y > Height-radius || circlePosition.y < radius)
+        {
+            direction.y *= -1f;
+        }
         //rörelsen
     }
 }
